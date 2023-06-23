fa35b69bf05413bdeab1cd2bdbc73e7772bc1550
dot.box <Mr.Bears2013@gmail.com>
2023-06-23 17:35:56 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,162 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import errors
+import aiohttp
+
+
+TOKEN = 'hidden'
+
+intents = discord.Intents.all()
+intents.message_content = True
+client = commands.Bot(command_prefix='/', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Bot {0.user}'.format(client) + ' is now online')
+
+
+@client.command()
+async def guide(ctx):
+    await ctx.send("Use /alert to ping a user")
+
+
+MAX_PINGS = 15  # Maximum number of pings allowed
+
+
+@client.command()
+async def alert(ctx, user: discord.Member, count: int):
+    if count <= 0:
+        await ctx.send("Please provide a positive number of pings.")
+        return
+
+    if not user.mention:
+        await ctx.send("bruh you forgot the @")
+        return
+    try:
+        if isinstance(ctx.channel, discord.Thread):
+            try:
+                for _ in range(count):
+                    await ctx.send(user.mention)
+            except errors.MemberNotFound:
+                await ctx.send("No user found, bozo")
+        else:
+            if count > MAX_PINGS:
+                await ctx.send(
+                    f"Only up to {MAX_PINGS} pings. Gets a bit glitchy after :pensive: \nThere is no limit in "
+                    f"threads")
+                return
+            for i in range(count):
+                await ctx.send(user.mention)
+
+                if i + 1 == MAX_PINGS:
+                    # Create a thread after reaching the maximum number of pings
+                    await ctx.send(f"Creating a thread for {user.display_name}...")
+                    thread = await ctx.channel.create_thread(name=f"Pings for {user.display_name}")
+    except errors.MemberNotFound:
+        await ctx.send("")
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, errors.MemberNotFound):
+        await ctx.send("no user found bozo")
+
+
+@client.command()
+async def setimage(ctx, url: str):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    image = await response.read()
+                    await client.user.edit(avatar=image)
+                    await ctx.send("Bot image has been updated successfully.")
+                else:
+                    await ctx.send("Failed to retrieve image from URL.")
+    except aiohttp.ClientError:
+        await ctx.send("Failed to update bot image.")
+
+
+client.run(TOKEN)
+import discord
+from discord.ext import commands
+from discord.ext.commands import errors
+import aiohttp
+
+
+TOKEN = 'hidden'
+
+intents = discord.Intents.all()
+intents.message_content = True
+client = commands.Bot(command_prefix='/', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Bot {0.user}'.format(client) + ' is now online')
+
+
+@client.command()
+async def guide(ctx):
+    await ctx.send("Use /alert to ping a user")
+
+
+MAX_PINGS = 15  # Maximum number of pings allowed
+
+
+@client.command()
+async def alert(ctx, user: discord.Member, count: int):
+    if count <= 0:
+        await ctx.send("Please provide a positive number of pings.")
+        return
+
+    if not user.mention:
+        await ctx.send("bruh you forgot the @")
+        return
+    try:
+        if isinstance(ctx.channel, discord.Thread):
+            try:
+                for _ in range(count):
+                    await ctx.send(user.mention)
+            except errors.MemberNotFound:
+                await ctx.send("No user found, bozo")
+        else:
+            if count > MAX_PINGS:
+                await ctx.send(
+                    f"Only up to {MAX_PINGS} pings. Gets a bit glitchy after :pensive: \nThere is no limit in "
+                    f"threads")
+                return
+            for i in range(count):
+                await ctx.send(user.mention)
+
+                if i + 1 == MAX_PINGS:
+                    # Create a thread after reaching the maximum number of pings
+                    await ctx.send(f"Creating a thread for {user.display_name}...")
+                    thread = await ctx.channel.create_thread(name=f"Pings for {user.display_name}")
+    except errors.MemberNotFound:
+        await ctx.send("")
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, errors.MemberNotFound):
+        await ctx.send("no user found bozo")
+
+
+@client.command()
+async def setimage(ctx, url: str):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    image = await response.read()
+                    await client.user.edit(avatar=image)
+                    await ctx.send("Bot image has been updated successfully.")
+                else:
+                    await ctx.send("Failed to retrieve image from URL.")
+    except aiohttp.ClientError:
+        await ctx.send("Failed to update bot image.")
+
+
+client.run(TOKEN)
