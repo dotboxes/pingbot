0f97925b3703b0c3fea6ce8e887db961c649185f
dot.box <Mr.Bears2013@gmail.com>
2023-07-08 22:05:50 -0400

    Ping roles!
    Added a way to ping roles now. Also added a say command for the bot owner. Now they can use an embed to announce things.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, Union
 import discord
 import aiohttp
 import asyncio
+
+from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import errors
 from discord.ext.commands import Greedy, Context
@@ -27,47 +29,54 @@ async def guide(interaction: discord.Interaction):
                                             "deletes bots threads\n"
                                             "/setimage - use link to set image")
 
-@client.command()
-async def alert(ctx, user: discord.Member, count: int):
-    guild_id = ctx.guild.id
 
+@client.command()
+async def alert(ctx, target: Union[discord.Role, discord.Member], count: int):
     if count <= 0:
         await ctx.send("Please provide a positive number of pings.")
         return
 
-    if not user.mention:
-        await ctx.send("bruh you forgot the @")
-        return
+    if isinstance(target, discord.Role):
+        mention_string = target.mention
 
-    try:
+        try:
+            for _ in range(count):
+                await ctx.send(mention_string)
+        except discord.errors.NotFound:
+            await ctx.send("No role found.")
+    elif isinstance(target, discord.Member):
+        if not target.mention:
+            await ctx.send("Please mention the user.")
+            return
+
         if isinstance(ctx.channel, discord.Thread):
             try:
                 for _ in range(count):
-                    await ctx.send(user.mention)
-            except errors.MemberNotFound:
-                await ctx.send("No user found, bozo")
+                    await ctx.send(target.mention)
+            except discord.errors.NotFound:
+                await ctx.send("No user found.")
         else:
+            MAX_PINGS = 15  # Maximum number of pings allowed
+            THREAD_DURATION = 2  # Duration in hours before thread deletion
+
             if count > MAX_PINGS:
-                await ctx.send(
-                    f"Only up to {MAX_PINGS} pings. Gets a bit glitchy after :pensive: \nThere is no limit in "
-                    f"threads")
+                await ctx.send(f"Only up to {MAX_PINGS} pings are allowed. It may get glitchy after that.")
                 return
+
             for i in range(count):
-                await ctx.send(user.mention)
+                await ctx.send(target.mention)
 
                 if i + 1 == MAX_PINGS:
-                    # Create a thread after reaching the maximum number of pings
-                    await ctx.send(f"Creating a thread for {user.display_name}...")
-                    thread = await ctx.channel.create_thread(name=f"Pings for {user.display_name}", private=False)
-                    # Schedule thread deletion after a specified duration
+                    await ctx.send(f"Creating a thread for {target.display_name}...")
+                    thread = await ctx.channel.create_thread(name=f"Pings for {target.display_name}", private=False)
                     await asyncio.sleep(THREAD_DURATION * 3600)  # Convert hours to seconds
                     await thread.delete()
-    except errors.MemberNotFound:
-        await ctx.send("")
+    else:
+        await ctx.send("Invalid target. Please mention a user or specify a role.")
+
 
 MAX_PINGS = 15  # Maximum number of pings allowed
 THREAD_DURATION = 2  # Duration in hours before thread deletion
-
 
 @client.event
 async def on_command_error(interaction: discord.Interaction, error):
@@ -123,9 +132,18 @@ async def setimage(interaction: discord.Interaction, url: str):
         await interaction.response.send_message("Failed to update bot image.")
 
 
+bot_owner_id = client.owner_id
+
+
+@client.command(aliases=['embedsay'])
+@commands.is_owner()
+async def say(ctx, title: str, description: str, *, color: discord.Color = discord.Color(0xFFFFFF)):
+    embed = discord.Embed(title=title, description=description, color=color)
+    await ctx.message.delete()
+    await ctx.send(embed=embed)
+
+
 # Sync Code
-
-
 @client.command()
 @commands.guild_only()
 @commands.is_owner()
