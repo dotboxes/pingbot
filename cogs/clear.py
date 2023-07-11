9a3d6be6567d4fb779c953f056138750e2aa314d
dot.box <Mr.Bears2013@gmail.com>
2023-07-11 17:39:58 -0400

    Fixed Clear
    Change client.user.id to self.client.user.id.

--- cogs/clear.py
+++ cogs/clear.py
@@ -7,10 +7,10 @@ class clear(commands.Cog):
     def __init__(self, client: commands.bot):
         self.client = client
 
-    @app_commands.command(name="guide", description="List of Commands")
-    async def clear(self, interaction: discord.Interaction):
+    @app_commands.command(name="clear", description="List of Commands")
+    async def clear(self, interaction: discord.Interaction, count: int = None):
         """Clears Messages"""
-        count = None;
+
         if count is None or count < 1:
             nmb = "you want to clear negative messages?"
             nmb_cb = f"```python\n{nmb}\n```"
@@ -18,8 +18,7 @@ class clear(commands.Cog):
             return
 
         def is_bot_message(message):
-            from main import client
-            return message.author.id == client.user.id and message != interaction.message
+            return message.author.id == self.client.user.id and message != interaction.message
 
         await interaction.response.defer()
 
